ae7ce02f7e21d78a0bd759ba079a1dfcd3295def
Ileen Fan <ifan2011@gmail.com>
2024-12-02 05:20:31 -0800

    add codspeed github action workflow

---
+++ aoc24/benches/bench.rs
@@ -0,0 +1,16 @@
+use codspeed_criterion_compat::{black_box, criterion_group, criterion_main, Criterion};
+
+use aoc24::day1::{part1, part2};
+
+fn bench_part1(c: &mut Criterion) {
+    let input = include_str!(".././src/data/day1.txt");
+    c.bench_function("part1", |b| b.iter(|| part1(black_box(input))));
+}
+
+fn bench_part2(c: &mut Criterion) {
+    let input = include_str!(".././src/data/day1.txt");
+    c.bench_function("part2", |b| b.iter(|| part2(black_box(input))));
+}
+
+criterion_group!(benches, bench_part1, bench_part2);
+criterion_main!(benches);
--- aoc24/src/main.rs
+++ aoc24/src/main.rs
@@ -2,6 +2,5 @@ pub mod day1;
 pub mod day2;
 
 fn main() {
-    
 }
 
